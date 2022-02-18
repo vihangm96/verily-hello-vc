cbbd436b331ee1c78de79be08eca25a0cdef79fc
Vihang Mangalvedhekar <vihangm96@gmail.com>
2022-02-18 16:49:40 -0700

    Update favorite_built_in_modules.py

--- favorite_built_in_modules.py
+++ favorite_built_in_modules.py
@@ -21,7 +21,7 @@
 import datetime
 import math
 import os
-
+import array
 
 def main():
     ###########################################################################
@@ -31,7 +31,7 @@ def main():
     ###########################################################################
     print("John DiMatteo likes datetime:", datetime.__doc__)
     print("Foo likes math:", math.__doc__)
-    print("Vihang likes os")
+    print("Vihang likes os and array")
 
 
 if __name__ == "__main__":
