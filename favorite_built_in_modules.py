4d0ce52ab55b156477c87783fb051bd66a0a117b
foo1111 <jdimatteo+foo@gmail.com>
2022-02-18 16:50:22 -0700

    Update favorite_built_in_modules.py

--- favorite_built_in_modules.py
+++ favorite_built_in_modules.py
@@ -22,6 +22,7 @@ import datetime
 import math
 import os
 import array
+import sqlite3
 
 
 def main():
@@ -33,7 +34,7 @@ def main():
     print("John DiMatteo likes datetime:", datetime.__doc__)
     print("Foo likes math:", math.__doc__)
     print("Vihang likes os")
-    print("Foo also likes array")
+    print("Foo also likes array and sqlite3")
 
 
 if __name__ == "__main__":
