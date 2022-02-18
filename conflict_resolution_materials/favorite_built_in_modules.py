8843dc99b025e1a88ce86fe271250d35109d6df5
John DiMatteo <jdimatteo@verily.com>
2022-02-18 13:51:47 -0700

    add file prone to conflicts

---
+++ conflict_resolution_materials/favorite_built_in_modules.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+"""Share some of your favoriate Python built in modules!
+"""
+
+###############################################################################
+#
+#   Step 1. Add a line importing a module you like here.
+#
+#   If you aren't sure, uncoment a line below (by deleting the preceding #):
+#
+#import decimal
+#import math
+#import itertools
+#import array
+#import bisect
+#import unittest
+#import sqlite3
+#
+###############################################################################
+import datetime
+
+
+def main():
+    ###########################################################################
+    #
+    #   Step 2. Print the module you like, and include the docstring.
+    #
+    ###########################################################################
+    print("John DiMatteo likes datetime:", datetime.__doc__)
+
+
+if __name__ == "__main__":
+    main()
